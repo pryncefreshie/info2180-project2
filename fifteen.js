a2a45da07f09c3094830065b0ccc9e84e566bbb7
Ackeem Smith <ackeem_Smith16@yahoo.com>
2018-10-30 20:23:58 -0500

    start state

--- fifteen.js
+++ fifteen.js
@@ -6,7 +6,29 @@ function start_state() {
     var puzzle_area = document.getElementById("puzzlearea").childNodes;
     var initial_state = [];
 
-    
+    var x = 0,
+        y = 0,
+        top = 0,
+        left = 0,
+        piece_counter = 1;
+
+    for (let i = 0; i < puzzle_area.length; i++) {
+        if (puzzle_area[i].nodeName == "DIV") {
+            initial_state.push([top.toString() + "px", left.toString() + "px"]);
+            puzzle_area[i].className += "puzzlepiece";
+            puzzle_area[i].setAttribute("style", `background-position: ${x}px ${y}px; top: ${top}px; left: ${left}px;`);
+            x -= 100;
+            left += 100;
+
+            if (piece_counter % 4 == 0) {
+                y -= 100;
+                top += 100;
+                left = 0
+            }
+            piece_counter += 1;
+
+        }
+    }
 
     return initial_state
 }
