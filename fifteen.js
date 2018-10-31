f6437f3f030ac8587d69f17ac0436d6dd6d2b69a
Ackeem Smith <ackeem_Smith16@yahoo.com>
2018-10-30 20:10:19 -0500

    Starting the fifteen.js file

---
+++ fifteen.js
@@ -0,0 +1,12 @@
+
+window.onload = main;
+
+//Maze piece Initialization function and returns initial maze state
+function start_state() {
+    var puzzle_area = document.getElementById("puzzlearea").childNodes;
+    var initial_state = [];
+
+    
+
+    return initial_state
+}
